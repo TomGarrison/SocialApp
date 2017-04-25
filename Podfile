0be12ee2b112c8d3fa6e7b13ee3d260ff89b32da
TomGarrison <Tomgarrison2011@gmail.com>
2017-04-25 15:44:11 -0600

    updates

--- Podfile
+++ Podfile
@@ -11,4 +11,6 @@ target 'socialapp' do
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
   pod 'FBSDKLoginKit'
+  pod 'SwiftKeychainWrapper'
+  
 end
--- socialapp/SignInVc.swift
+++ socialapp/SignInVc.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVc: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func facebookButtonClicked(_ sender: Any) {
+        print("HELLO")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    @IBAction func facebookButtonClicked(_ sender: Any) {
+//        
+//        let facebookLogin = FBSDKLoginManager()
+//        
+//        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+//            
+//            if error != nil {
+//                
+//                print("Unable to authenticate with Facebook")
+//            }else if result?.isCancelled == true{
+//                
+//                print("User cancelled Facebook authentication")
+//            }else{
+//                
+//                print("Successfully authenticated with Facebook")
+//                
+//                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+//                
+////                self.firebaseAuth(credential)
+//            }
+//        }
+//    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        
+//        // Dispose of any resources that can be recreated.
+//    }
 
-
+    
+//    func firebaseAuth(_ credential: FIRAuthCredential){
+//       
+//        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+//            
+//            if error != nil {
+//                
+//                print("Unable to authenticat with Firebase")
+//            
+//            }else {
+//                
+//                print("Successfully authenticated with Firebase")
+//            }
+//        })
+//    }
 }
 
